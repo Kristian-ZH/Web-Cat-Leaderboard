dfe56c7d8e0eefb3564864a47b1d946d09ad2499
KristianZH <kristianzh@abv.bg>
2019-07-20 19:10:35 +0300

    Add total scores

--- main.go
+++ main.go
@@ -2,18 +2,33 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"text/template"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+const (
+	ScoreTableOffseet  = 2
+	HeadersTableOffset = 1
+)
+
+var (
+	courseName = os.Getenv("COURSE_NAME") //COURSE_NAME=MJT2019
+	dbUser     = os.Getenv("DB_USERNAME")
+	dbPass     = os.Getenv("DB_PASSWORDE")
+	dbHost     = os.Getenv("DB_HOST")
+	dbName     = os.Getenv("DB_NAME")
+)
+
 type SubmissionResult struct {
 	UserName   string
 	Assignment string
-	score      float64
+	Score      float64
 }
 
 type User struct {
@@ -23,12 +38,7 @@ type User struct {
 }
 
 func dbConn() (db *sql.DB) {
-	dbDriver := "mysql"
-	dbUser := ""
-	dbPass := ""
-	dbName := ""
-	db, err := sql.Open(dbDriver, dbUser+":"+dbPass+"@/"+dbName)
-	db.SetMaxIdleConns(0)
+	db, err := sql.Open(dbHost, dbUser+":"+dbPass+"@/"+dbName)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -42,23 +52,27 @@ func Index(w http.ResponseWriter, r *http.Request) {
 	userNames := getUserNames()
 	table := make([][]string, len(userNames)+1)
 	for i := range table {
-		table[i] = make([]string, len(assignmentNames)+1)
+		table[i] = make([]string, len(assignmentNames)+ScoreTableOffseet)
 	}
 
+	table[0][1] = "Total"
 	for idx, assignment := range assignmentNames {
-		table[0][idx+1] = assignment
+		table[0][idx+ScoreTableOffseet] = assignment
 	}
 	for idx, user := range userNames {
 		table[idx+1][0] = user.FirstName + " " + user.LastName
 	}
 
 	db := dbConn()
-	selectQuery := "SELECT user.OID, user.CFIRSTNAME, user.CLASTNAME ,ass.CASSIGNMENTNAME, ( COALESCE(subresult.CCORRECTNESSSCORE, 0) + COALESCE(subresult.CTOOLSCORE, 0) + COALESCE(subresult.CTASCORE ,0)) AS score " +
-		"FROM TSUBMISSION sub " +
-		"INNER JOIN TSUBMISSIONRESULT subresult ON subresult.OID=sub.CRESULTID " +
-		"INNER JOIN TASSIGNMENT ass ON ass.OID=sub.CASSIGNMENTID " +
-		"INNER JOIN TUSER user ON user.OID=sub.CUSERID " +
-		"WHERE subresult.CISMOSTRECENT='1';"
+	selectQuery := fmt.Sprintf("SELECT user.OID, user.CFIRSTNAME, user.CLASTNAME ,ass.CASSIGNMENTNAME, ( COALESCE(subresult.CCORRECTNESSSCORE, 0) + COALESCE(subresult.CTOOLSCORE, 0) + COALESCE(subresult.CTASCORE ,0)) AS score "+
+		"FROM TSUBMISSION sub "+
+		"INNER JOIN TSUBMISSIONRESULT subresult ON subresult.OID=sub.CRESULTID "+
+		"INNER JOIN TASSIGNMENT ass ON ass.OID=sub.CASSIGNMENTID "+
+		"INNER JOIN TUSER user ON user.OID=sub.CUSERID "+
+		"INNER JOIN TCOURSESTUDENT cu ON user.OID=cu.CID1 "+
+		"INNER JOIN TCOURSEOFFERING coff ON cu.CID=coff.OID "+
+		"INNER JOIN TCOURSE course ON coff.CCOURSEID=course.OID "+
+		"WHERE subresult.CISMOSTRECENT='1' AND ass.CASSIGNMENTURL IS NOT NULL AND user.CACCESSLEVEL='0' AND coff.CCRN='%s';", courseName)
 	selDB, err := db.Query(selectQuery)
 	if err != nil {
 		panic(err.Error())
@@ -78,13 +92,22 @@ func Index(w http.ResponseWriter, r *http.Request) {
 		saveSubmissionInTable(&user, assignmentName, score, &table, len(userNames), len(assignmentNames))
 	}
 
+	applyTotalScores(&table, len(userNames), len(assignmentNames))
+	sortScores(&table, len(userNames))
+
 	tmpl.ExecuteTemplate(w, "Index", table)
 	defer db.Close()
 }
 
 func getUserNames() []User {
 	db := dbConn()
-	selDB, err := db.Query("SELECT OID, CFIRSTNAME, CLASTNAME FROM TUSER;")
+	selectQuery := fmt.Sprintf("SELECT user.OID, user.CFIRSTNAME, user.CLASTNAME "+
+		"FROM TUSER user "+
+		"INNER JOIN TCOURSESTUDENT cu ON user.OID=cu.CID1 "+
+		"INNER JOIN TCOURSEOFFERING coff ON cu.CID=coff.OID "+
+		"INNER JOIN TCOURSE course ON coff.CCOURSEID=course.OID "+
+		"WHERE CACCESSLEVEL='0' AND coff.CCRN='%s';", courseName)
+	selDB, err := db.Query(selectQuery)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -108,7 +131,13 @@ func getUserNames() []User {
 
 func getAssignments() []string {
 	db := dbConn()
-	selDB, err := db.Query("SELECT CASSIGNMENTNAME FROM TASSIGNMENT ORDER BY OID;")
+	selectQuery := fmt.Sprintf("SELECT ass.CASSIGNMENTNAME "+
+		"FROM TASSIGNMENT ass "+
+		"INNER JOIN TASSIGNMENTOFFERING assoff ON ass.OID=assoff.CASSIGNMENTID "+
+		"INNER JOIN TCOURSEOFFERING coff ON assoff.CCOURSEOFFERINGID=coff.OID "+
+		"WHERE CASSIGNMENTURL IS NOT NULL AND coff.CCRN='%s'"+
+		"ORDER BY coff.OID;", courseName)
+	selDB, err := db.Query(selectQuery)
 	if err != nil {
 		panic(err.Error())
 	}
@@ -133,7 +162,7 @@ func saveSubmissionInTable(user *User, assignmentName string, score float64, tab
 
 func findUserIndex(user *User, table *[][]string, usersNum int) int {
 	userIndex := 0
-	for i := 1; i <= usersNum; i++ {
+	for i := HeadersTableOffset; i < usersNum+HeadersTableOffset; i++ {
 		if (*table)[i][0] == user.FirstName+" "+user.LastName {
 			userIndex = i
 			break
@@ -145,7 +174,7 @@ func findUserIndex(user *User, table *[][]string, usersNum int) int {
 
 func findAssignmentIndex(assignmentName string, table *[][]string, assignmentsNum int) int {
 	assignmentIndex := 0
-	for j := 1; j <= assignmentsNum; j++ {
+	for j := HeadersTableOffset; j < assignmentsNum+ScoreTableOffseet; j++ {
 		if (*table)[0][j] == assignmentName {
 			assignmentIndex = j
 			break
@@ -153,6 +182,33 @@ func findAssignmentIndex(assignmentName string, table *[][]string, assignmentsNu
 	}
 
 	return assignmentIndex
+}
+
+func applyTotalScores(table *[][]string, usersNum, assignmentsNum int) {
+	for i := HeadersTableOffset; i < usersNum+HeadersTableOffset; i++ {
+		var userTotal float64
+		for j := ScoreTableOffseet; j < assignmentsNum+ScoreTableOffseet; j++ {
+			if score, err := strconv.ParseFloat((*table)[i][j], 64); err == nil {
+				userTotal += score
+			}
+		}
+		(*table)[i][HeadersTableOffset] = strconv.FormatFloat(userTotal, 'f', 1, 64)
+	}
+}
+
+func sortScores(table *[][]string, usersNum int) {
+	scoreIndex := 1
+	for i := HeadersTableOffset + 1; i < usersNum+HeadersTableOffset; i++ {
+		j := i
+		for j > 1 {
+			currentScore, _ := strconv.ParseFloat((*table)[j][scoreIndex], 64)
+			previousScore, _ := strconv.ParseFloat((*table)[j-1][scoreIndex], 64)
+			if previousScore < currentScore {
+				(*table)[j-1], (*table)[j] = (*table)[j], (*table)[j-1]
+			}
+			j--
+		}
+	}
 }
 
 func main() {
